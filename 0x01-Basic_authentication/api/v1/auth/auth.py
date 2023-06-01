4dad30c313f8ead390689aa32999f8a2877f8730
Dr-MarcusI <oshomarcus20@gmail.com>
2023-05-31 19:33:06 -0700

    bs

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""API authentication.
+"""
+import re
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Returns Autentication for now.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
+        """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns the value of the Authorization header
+        from the Flask request object.
+        """
+        if request is None:
+            return None
+
+        auth_header = request.headers.get('Authorization')
+        if auth_header is None:
+            return None
+
+        return auth_header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None for now.
+        """
+        return None
